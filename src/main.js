b2e9fbfbd193928f81acbc494921e1c677734dcc
GaoYang <gy148485@163.com>
2019-03-12 15:33:19 +0800

    '关联远程库'

--- src/main.js
+++ src/main.js
@@ -7,10 +7,13 @@ import * as fundebug from "fundebug-javascript";
 import fundebugVue from "fundebug-vue";
 fundebug.apikey = "9816ef7058e3c108c57cb5c774f3b6141edf5b53ce3e6704d84b7da7df71c573"
 fundebugVue(fundebug, Vue);
+// 引入vant所有组件
+import Vant from 'vant';
+import 'vant/lib/index.css';
 
 // vant ui 组件库文档
 // https://youzan.github.io/vant/#/zh-CN/intro
-
+Vue.use(Vant);
 Vue.config.productionTip = false
 
 let vm = new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,11 @@
 <template>
 	<div class="home">
-		<div class="radio">
-			<div class="radius" checked  ref="male">
-				<input type="radio" name='sex' value='male' id='male' checked="checked">
-			</div>
-			<label for='male'>男</label>
-			<div class="radius">
-				<input type="radio" name='sex' value='female' id='female' ref="female">
-			</div>
-			<label for="female">女</label>
-		</div>
+		<van-tabs v-model="active">
+			<van-tab title="标签 1">内容 1</van-tab>
+			<van-tab title="标签 2">内容 2</van-tab>
+			<van-tab title="标签 3">内容 3</van-tab>
+			<van-tab title="标签 4">内容 4</van-tab>
+		</van-tabs>
 	</div>
 </template>
 
@@ -21,15 +17,14 @@
 		components: {
 
 		},
-		data(){
+		data() {
 			return {
-				sex:['男','女'],
-				isClick:false
+				active: 2
 			}
 		},
-		methods:{
-			bindChecked(el){
-				
+		methods: {
+			bindChecked(el) {
+
 			}
 		}
 	}
@@ -46,18 +41,20 @@
 			height: 50px;
 			display: flex;
 			padding: 30px;
-			.radius{
+
+			.radius {
 				position: relative;
 				width: 30px;
 				height: 30px;
 				background-color: #00a4ff;
 				border-radius: 50%;
-				&::before{
+
+				&::before {
 					content: "";
 					position: absolute;
 					display: block;
 					width: 60%;
-					height:60%;
+					height: 60%;
 					background-color: red;
 					top: 0;
 					left: 0;
@@ -66,20 +63,22 @@
 					margin: auto;
 					border-radius: 50%;
 				}
-				&[checked]{
+
+				&[checked] {
 					background-color: yellow;
 				}
+
 				input[type=radio] {
 					width: 30px;
 					height: 30px;
 					opacity: .1;
 				}
-				
+
 				input[checked] {
 					background-color: red;
 				}
 			}
-			
+
 		}
 	}
 </style>
