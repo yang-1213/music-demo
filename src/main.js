d837647289de80b4f5d8a04661712dddaa60fc93
GaoYang <gy148485@163.com>
2019-03-12 14:57:22 +0800

    '初始化项目结构'

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './router/index.js'
+import store from './store/index.js'
+// 引入fundebug
+import * as fundebug from "fundebug-javascript";
+import fundebugVue from "fundebug-vue";
+fundebug.apikey = "9816ef7058e3c108c57cb5c774f3b6141edf5b53ce3e6704d84b7da7df71c573"
+fundebugVue(fundebug, Vue);
+
+// vant ui 组件库文档
+// https://youzan.github.io/vant/#/zh-CN/intro
 
 Vue.config.productionTip = false
 
-new Vue({
+let vm = new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+console.log(vm);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,85 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<div class="radio">
+			<div class="radius" checked  ref="male">
+				<input type="radio" name='sex' value='male' id='male' checked="checked">
+			</div>
+			<label for='male'>男</label>
+			<div class="radius">
+				<input type="radio" name='sex' value='female' id='female' ref="female">
+			</div>
+			<label for="female">女</label>
+		</div>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+	// @ is an alias to /src
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: 'home',
+		components: {
+
+		},
+		data(){
+			return {
+				sex:['男','女'],
+				isClick:false
+			}
+		},
+		methods:{
+			bindChecked(el){
+				
+			}
+		}
+	}
 </script>
+
+
+<style scoped lang='scss'>
+	.home {
+		background-color: lightskyblue;
+		font-size: 30px;
+
+		.radio {
+			width: 50%;
+			height: 50px;
+			display: flex;
+			padding: 30px;
+			.radius{
+				position: relative;
+				width: 30px;
+				height: 30px;
+				background-color: #00a4ff;
+				border-radius: 50%;
+				&::before{
+					content: "";
+					position: absolute;
+					display: block;
+					width: 60%;
+					height:60%;
+					background-color: red;
+					top: 0;
+					left: 0;
+					right: 0;
+					bottom: 0;
+					margin: auto;
+					border-radius: 50%;
+				}
+				&[checked]{
+					background-color: yellow;
+				}
+				input[type=radio] {
+					width: 30px;
+					height: 30px;
+					opacity: .1;
+				}
+				
+				input[checked] {
+					background-color: red;
+				}
+			}
+			
+		}
+	}
+</style>
